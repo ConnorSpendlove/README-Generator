f6dc6a4055293fc95a9b3a20557ca509c6ef1419
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-02-29 01:41:08 -0700

    Installed npm inquirer, and required all necessary packages in index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// TODO: Include packages needed for this application
+// Requires all necessary packages needed. 
+// inquirer is used to make user input more versatile in node.
+// fs allows the file structure of the app to be altered.
+// generateMarkdown links information occuring in a separate js file
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [];
